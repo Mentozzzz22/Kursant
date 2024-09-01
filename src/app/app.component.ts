68cc51e39ad9cc189abf90fc2853b293ebd63d41
AbayMendybayev22 <Mentoz2205@>
2024-09-02 03:07:34 +0500

    navbar finished, main page 30%

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {CardModule} from "primeng/card";
+import {Button} from "primeng/button";
+import {NavbarComponent} from "./navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CardModule, Button, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
