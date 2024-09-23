bbdecf608a70eff9c15831b425e48d6a8a902ae0
AbayMendybayev22 <Mentoz2205@>
2024-09-24 02:31:11 +0500

    homework almost finished

--- src/app/student/home-work/home-work.component.ts
+++ src/app/student/home-work/home-work.component.ts
@@ -26,33 +26,25 @@ interface Homework {
   templateUrl: './home-work.component.html',
   styleUrl: './home-work.component.css'
 })
-export class HomeWorkComponent implements OnInit{
+export class HomeWorkComponent implements OnInit {
 
   private learnerHomeworkService = inject(LearnerHomeworkService);
   private router = inject(Router)
   private route = inject(ActivatedRoute)
   private messageService = inject(MessageService);
 
-  learnerHomeWorks:LearnerHomeworks[]=[];
+  learnerHomeWorks: LearnerHomeworks[] = [];
 
-
-  homeworks: Homework[] = [
-    { subject: 'Тарих', description: 'Адамзаттың пайда болуы', date: '09/09/2024', time: '23:59', score: '89/100', status: 'completed' },
-    { subject: 'Математика', description: 'Адамзаттың пайда болуы', date: '09/09/2024', time: '23:59', score: '--/100', status: 'incomplete' },
-    { subject: 'Физика', description: 'Адамзаттың пайда болуы', date: '09/09/2024', time: '23:59', score: '--/100', status: 'pending' },
-  ];
-
-  isLate(date: string, time: string): boolean {
-    const homeworkDate = new Date(`${date} ${time}`);
-    return homeworkDate < new Date();
+  isLate(deadline: string): boolean {
+    return true
   }
 
   ngOnInit(): void {
     this.loadLearnerHomeWork()
   }
 
-  loadLearnerHomeWork(){
-    this.learnerHomeworkService.getLearnerHomeworks().subscribe(data=> {
+  loadLearnerHomeWork() {
+    this.learnerHomeworkService.getLearnerHomeworks().subscribe(data => {
       this.learnerHomeWorks = data;
     })
   }
